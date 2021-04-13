161deb1c12c6ef31af9e43a1fc293a3f7b9b2d8e
Matt Ruffalo <matt.ruffalo@gmail.com>
2021-04-13 19:58:32 -0400

    Enable manhole in SPRM step

--- test.sh
+++ test.sh
@@ -9,5 +9,5 @@ mkdir -p $DIRNAME/output
 cd $DIRNAME/output
 
 cwltool ../codex-pipeline/pipeline.cwl --data_dir $1
-cwltool ../sprm/pipeline.cwl --image_dir stitched/expressions --mask_dir stitched/mask
+cwltool ../sprm/pipeline.cwl --enable_manhole --image_dir stitched/expressions --mask_dir stitched/mask
 cwltool ../create-vis-symlink-archive/pipeline.cwl --ometiff_dir stitched --sprm_output sprm_outputs
