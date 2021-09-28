7acf6127596d1fe0bfad90ab79e58842edce38a1
Matt Ruffalo <matt.ruffalo@gmail.com>
2021-09-28 11:11:31 -0400

    "$1"

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p $DIRNAME/output
 cd $DIRNAME/output
 
-cwltool --timestamps ../codex-pipeline/pipeline.cwl --data_dir $1
+cwltool --timestamps ../codex-pipeline/pipeline.cwl --data_dir "$1"
 cwltool --timestamps ../sprm/pipeline.cwl --enable_manhole --image_dir stitched/expressions --mask_dir stitched/mask
 cwltool --timestamps ../create-vis-symlink-archive/pipeline.cwl --ometiff_dir stitched --sprm_output sprm_outputs
